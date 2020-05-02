77a6737dd90dd7d551844374f02a78d6e080c805
Gorokhovsky <55406504+Gorokhovsky@users.noreply.github.com>
2020-05-02 14:30:00 +0300

    finish

---
+++ Menu Builder/Class_Managment/Class__Managment.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Reflection;
+using Menu_Builder.MyClasses;
+
+namespace Menu_Builder.Class_Managment
+{
+    class Class__Managment
+    {
+        Object[] objects;
+        Type[] types;
+        int edit_obj_num;
+        int edit_prop_num;
+        PropertyInfo[] edit_obj_props;
+
+        public Class__Managment()
+        {
+            Array.Resize(ref objects, 0);
+            Assembly assembly = Assembly.Load("Menu Builder");
+            types = assembly.GetTypes();
+
+        }
+        public int GetEditObjNum()
+        {
+            return edit_obj_num;
+        }
+
+        public int GetObjNum()
+        {
+            return objects.Length; ;
+        }
+
+        public int GetEditPropNum()
+        {
+            return edit_prop_num;
+        }
+
+        public string[] GetClasses()
+        {
+            string[] result = new string[types.Length];
+            int j = 0;
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (types[i].IsClass && (!types[i].IsAbstract) && types[i].FullName.Contains("MyClasses"))
+                    result[j++] = types[i].Name;
+            }
+            Array.Resize(ref result, j);
+            return result;
+        }  
+
+        public object CreateObject(string obj_name)
+        {
+            int objs_len = objects.Length;
+            Array.Resize(ref objects, objs_len + 1);
+            int i = 0;
+            while (obj_name != types[i].Name)
+            {
+                i++;
+            }
+            objects[objs_len] = Activator.CreateInstance(types[i]);
+            return objects[objs_len];
+        }
+
+        public string GetName()
+        {
+            Type type = objects[edit_obj_num].GetType();
+            PropertyInfo prop_info = type.GetProperty("name", BindingFlags.Instance
+                    | BindingFlags.Static
+                    | BindingFlags.Public
+                    | BindingFlags.NonPublic);
+            return (string)prop_info.GetValue(objects[edit_obj_num]);////??
+        }
+
+        public void SetActiveLastObj()
+        {
+            edit_obj_num = objects.Length - 1;
+            edit_prop_num = 0;
+            GetProps();
+        }
+        public void SetActiveObj(int index)
+        {
+            edit_obj_num = index;
+            edit_prop_num = 0;
+            GetProps();
+        }
+
+        public void SetActiveProp(int num)
+        {
+            edit_prop_num = num;
+        }
+
+        public bool IsPropClass()
+        {
+            string a = edit_obj_props[edit_prop_num].PropertyType.Name;
+            return edit_obj_props[edit_prop_num].PropertyType.IsClass &&
+                edit_obj_props[edit_prop_num].PropertyType.Name != "String";
+        }
+        public void GetProps()
+        {
+            Type type = objects[edit_obj_num].GetType();
+            edit_obj_props=type.GetProperties(BindingFlags.Instance
+                    | BindingFlags.Static
+                    | BindingFlags.Public
+                    | BindingFlags.NonPublic);
+        }
+
+        public string[] GetObjPropsTxt()
+        {
+            string[] result = new string[edit_obj_props.Length];
+            for (int i = 0; i < edit_obj_props.Length; i++)
+            {
+                result[i] = edit_obj_props[i].Name + " :  ";
+                if (edit_obj_props[i].GetValue(objects[edit_obj_num]) != null)
+                    if (edit_obj_props[i].PropertyType.IsClass &&
+                    edit_obj_props[i].PropertyType.Name != "String")
+                    {
+                        Type type = objects[edit_obj_num].GetType();
+                        PropertyInfo prop_info = type.GetProperty("menu_item", BindingFlags.Instance
+                                | BindingFlags.Static
+                                | BindingFlags.Public
+                                | BindingFlags.NonPublic);
+                        object buf = prop_info.GetValue(objects[edit_obj_num]);
+                        type = buf.GetType();
+                        prop_info = type.GetProperty("name", BindingFlags.Instance
+                                | BindingFlags.Static
+                                | BindingFlags.Public
+                                | BindingFlags.NonPublic);
+                        result[i] += (string)prop_info.GetValue(buf);
+                    }
+                    else
+                        result[i] += Convert.ToString(edit_obj_props[i].GetValue(objects[edit_obj_num])) + ";";
+                else
+                    result[i] += "none;";
+
+
+            }
+            return result;
+        }
+
+        private object[] GetChildObjects()
+        {
+            object[] result = new object[0];
+            for (int i = 0; i < objects.Length; i++)
+                if (objects[i].GetType().BaseType == edit_obj_props[edit_prop_num].PropertyType)
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[result.Length - 1] = objects[i];
+                }
+            return result;
+        }
+        public string[] GetChildObjectsTxt()
+        {
+            object[] children = GetChildObjects();
+            string[] result = new string[children.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                Type type = children[i].GetType();
+                PropertyInfo prop_info = type.GetProperty("name", BindingFlags.Instance
+                        | BindingFlags.Static
+                        | BindingFlags.Public
+                        | BindingFlags.NonPublic);
+                result[i] = (string)prop_info.GetValue(children[i]);
+            }
+            return result;
+        }
+        public bool SetPropVal(string val, int ind)
+        {
+            if (edit_obj_props[edit_prop_num].PropertyType.Name == "Int32")
+            {
+                try
+                {
+                    if (Convert.ToInt32(val) >= 0)
+                    {
+                        edit_obj_props[edit_prop_num].SetValue(objects[edit_obj_num],
+                            Convert.ToInt32(val));
+                        return true;
+                    }
+                    else return false;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            if (edit_obj_props[edit_prop_num].PropertyType.Name == "String")
+            {
+                edit_obj_props[edit_prop_num].SetValue(objects[edit_obj_num], val);
+                return true;
+            }
+            if (edit_obj_props[edit_prop_num].PropertyType.IsClass &&
+                edit_obj_props[edit_prop_num].PropertyType.Name != "String")
+            {
+                object[] children = GetChildObjects();
+                edit_obj_props[edit_prop_num].SetValue(objects[edit_obj_num], children[ind]);
+                return true;
+                //edit_obj_props[edit_prop_num].SetValue(objects[edit_obj_num], CreateObject(val));
+            }
+            return false;
+        }
+
+        public void DeleteObj()
+        {
+            PropertyInfo[] buf_props;
+            Type type;
+            for (int i = 0; i < objects.Length; i++)
+            {
+                type = objects[i].GetType();
+                buf_props = type.GetProperties(BindingFlags.Instance
+                        | BindingFlags.Static
+                        | BindingFlags.Public
+                        | BindingFlags.NonPublic);
+                for (int j = 0; j < buf_props.Length; j++)
+                    if (ReferenceEquals(buf_props[j].GetValue(objects[i]), objects[edit_obj_num]))
+                    {
+                        buf_props[j].SetValue(objects[i], null);
+                    }
+            }
+            objects[edit_obj_num] = null;
+            for (int i = edit_obj_num; i < objects.Length - 1; i++)
+            {
+                objects[i] = objects[i + 1];
+            }
+            Array.Resize(ref objects, objects.Length - 1);
+            if (edit_obj_num > 0)
+                edit_obj_num -= 1;
+        }
+    }
+}
---
+++ Menu Builder/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Menu_Builder.Class_Managment;
+
+namespace Menu_Builder
+{
+    public partial class MenuBuilder_form : Form
+    {
+        private Class_Managment.Class__Managment class_managment;
+        public MenuBuilder_form()
+        {
+            InitializeComponent();
+            class_managment = new Class__Managment();
+            string[] buf = class_managment.GetClasses();
+            ClassesCB.Items.Clear();
+            for (int i = 0; i < buf.Length; i++)
+            {
+                ClassesCB.Items.Add(buf[i]);
+            }
+            ConfirmBtn.Enabled = false;
+            PropsCB.Enabled = false;
+            PropsTB.Enabled = false;
+        }
+
+        private void UpdateObjs()
+        {
+            ObjectsLB.Items.Clear();
+            int buf = class_managment.GetEditObjNum();
+            for (int i=0;i < class_managment.GetObjNum(); i++)
+            {
+                class_managment.SetActiveObj(i);
+                ObjectsLB.Items.Add(class_managment.GetName());
+            }
+            if (class_managment.GetObjNum() > 0)
+            {
+                class_managment.SetActiveObj(buf);
+                ObjectsLB.SelectedIndex = class_managment.GetEditObjNum();
+            }
+        }
+
+        private void UpdateProps()
+        {
+            PropertiesLB.Items.Clear();
+            ConfirmBtn.Enabled = false;
+            if (ObjectsLB.SelectedIndex!=-1)
+            {
+                string[] obj_info = class_managment.GetObjPropsTxt();
+                for (int i = 0; i < obj_info.Length; i++)
+                    PropertiesLB.Items.Add(obj_info[i]);
+                PropertiesLB.SelectedIndex = class_managment.GetEditPropNum();
+                ConfirmBtn.Enabled = true;
+                PropsTB.Enabled = true;
+                PropsCB.Enabled = true;
+            }
+        }
+        private void ClassesCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+     
+
+        private void ObjectsLB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            class_managment.SetActiveObj(ObjectsLB.SelectedIndex);
+            UpdateProps();
+        }
+
+        private void CreateBtn_Click(object sender, EventArgs e)
+        {
+            if (ClassesCB.Text == "")
+            {
+                MessageBox.Show("Please, choose a class", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                class_managment.CreateObject(ClassesCB.Text);
+                class_managment.SetActiveLastObj();
+                UpdateObjs();
+                ObjectsLB.SelectedIndex = class_managment.GetEditObjNum();
+
+            }
+        }
+
+        private void PropertiesLB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            class_managment.SetActiveProp(PropertiesLB.SelectedIndex);
+            PropsCB.Visible = class_managment.IsPropClass();
+            PropsTB.Visible = !class_managment.IsPropClass();
+            PropsCB.Items.Clear();
+            PropsTB.Clear();
+            if (PropsCB.Enabled)
+            {
+                string[] buf = class_managment.GetChildObjectsTxt();
+                for (int i = 0; i < buf.Length; i++)
+                {
+                    PropsCB.Items.Add(buf[i]);
+                }
+            }
+        }
+
+        private void PropsCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConfirmBtn_Click(object sender, EventArgs e)
+        {
+            if (PropsCB.Visible)
+                class_managment.SetPropVal(PropsCB.Text, PropsCB.SelectedIndex);
+            else
+            {
+                if (!class_managment.SetPropVal(PropsTB.Text, 0))
+                {
+                    MessageBox.Show("Wrong value!", "Message", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            UpdateObjs();
+            UpdateProps();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+                if (ObjectsLB.SelectedIndex == -1)
+                    MessageBox.Show("No objects selected!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                {
+                    class_managment.DeleteObj();
+                    UpdateObjs();
+                }
+            }
+    }
+}
